648592e58ae8a52270de3c5c26cb62c367cb40d4
Nazar Levkovych <nazar.levkovich.2018@gmail.com>
2024-11-29 15:20:24 +0200

    test auth

--- src/app/(auth)/sign/in/page.tsx
+++ src/app/(auth)/sign/in/page.tsx
@@ -29,21 +29,7 @@ export default function Page() {
           label="Password"
           labelPlacement="outside"
           placeholder="••••••••••"
-          classNames={{
-            label: "text-md group-data-[filled-within=true]:text-whisperGray pb-[6px]",
-            input: ["text-md group-data-[has-value=true]:text-dustyBlue"],
-            inputWrapper: [
-              "bg-charcoalBlue",
-              "h-12",
-              "outline-slateBlue",
-              "outline-offset-0",
-              "outline-[1px]",
-              "data-[hover=true]:bg-charcoalBlue",
-              "group-data-[hover=true]:outline-none",
-              "group-data-[focus=true]:bg-white",
-              "group-data-[focus=true]:outline-none",
-            ],
-          }}
+          classNames={inputClassNames}
         />
       </div>
 
